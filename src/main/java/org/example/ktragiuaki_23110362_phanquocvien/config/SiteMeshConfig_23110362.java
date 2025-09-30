16c6d784a222d99a9593674e49a679a374868de3
Vien Phan <phanquocvien.123@gmail.com>
2025-09-30 16:07:27 +0700

    update sitemesh

--- src/main/java/org/example/ktragiuaki_23110362_phanquocvien/config/SiteMeshConfig_23110362.java
+++ src/main/java/org/example/ktragiuaki_23110362_phanquocvien/config/SiteMeshConfig_23110362.java
@@ -1,3 +1,21 @@
 package org.example.ktragiuaki_23110362_phanquocvien.config;
 
-// Sitemesh removed
+import org.sitemesh.builder.SiteMeshFilterBuilder;
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
+
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(filterName = "sitemesh", urlPatterns = {"/*"})
+public class SiteMeshConfig_23110362 extends ConfigurableSiteMeshFilter {
+    @Override
+    protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
+        builder
+                .addDecoratorPath("/admin/*", "/WEB-INF/views/decorators/admin.jsp")
+                .addDecoratorPath("/*", "/WEB-INF/views/decorators/user.jsp")
+                .addExcludedPath("/assets/*")
+                .addExcludedPath("/static/*")
+                .addExcludedPath("/WEB-INF/views/decorators/*")
+                .addExcludedPath("/login")
+                .addExcludedPath("/register");
+    }
+}
